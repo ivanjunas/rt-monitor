3696e9cbfc0359517ce6df450e473e5c10492240
Ivan Junas <ivanjunas22@gmail.com>
2017-10-29 18:04:14 +0100

    add faye server and client generating dummy data

---
+++ src/publishTrafic.js
@@ -0,0 +1,33 @@
+
+// usage: node server [address] [port] 
+
+var faye = require('faye'),
+    address = process.argv[2] || '127.0.0.1',
+    port = process.argv[3] || '8000';
+
+function getTimeValue() {
+  var dateBuffer = new Date();
+  var Time = dateBuffer.getTime();
+  return Time;
+}
+
+function getRandomValue() {
+  var randomValue = Math.random() * 100;
+  return randomValue;
+}
+
+var endpoint = 'http://' + address + ':' + port + '/bayeux',
+    client   = new faye.Client(endpoint),
+    n        = 0;
+
+// send out traffic second
+setInterval(function() {
+
+  // load data from the perl script 
+
+  client.publish('/traffic', {
+    user: 'ivan', 
+    data: [{time: getTimeValue(), y:getRandomValue()}]
+  });
+
+}, 1000);
---
+++ src/server.js
@@ -0,0 +1,63 @@
+// usage: node server [port] [tls] 
+var fs      = require('fs'),
+    path    = require('path'),
+    http    = require('http'),
+    https   = require('https'),
+    mime    = require('mime'),
+    deflate = require('permessage-deflate'),
+    faye    = require('faye');
+
+var SHARED_DIR = __dirname,
+    PUBLIC_DIR = SHARED_DIR + '/public',
+
+    // addapeter na faye 
+    bayeux     = new faye.NodeAdapter({mount: '/bayeux', timeout: 20}),
+
+    port       = process.argv[2] || '8000',
+    secure     = process.argv[3] === 'tls',
+    key        = fs.readFileSync(SHARED_DIR + '/server.key'),
+    cert       = fs.readFileSync(SHARED_DIR + '/server.crt');
+
+bayeux.addWebsocketExtension(deflate);
+
+
+// normal handling of request, doesn't have anything with faye 
+var handleRequest = function(request, response) {
+  var path = (request.url === '/') ? '/index.html' : request.url;
+
+  fs.readFile(PUBLIC_DIR + path, function(err, content) {
+    var status = err ? 404 : 200;
+    try {
+      response.writeHead(status, {'Content-Type': mime.lookup(path)});
+      response.end(content || 'Not found');
+    } catch (e) {}
+  });
+};
+
+var server = secure
+           ? https.createServer({cert: cert, key: key}, handleRequest)
+           : http.createServer(handleRequest);
+
+bayeux.attach(server);
+server.listen(Number(port));
+
+bayeux.getClient().subscribe('/traffic', function(msg) {
+  console.log('[' + msg.user + ']: ' + JSON.stringify(msg.data));
+});
+
+
+// logging of subscribtions and connections
+
+bayeux.on('subscribe', function(clientId, channel) {
+  console.log('[  SUBSCRIBE] ' + clientId + ' -> ' + channel);
+});
+
+bayeux.on('unsubscribe', function(clientId, channel) {
+  console.log('[UNSUBSCRIBE] ' + clientId + ' -> ' + channel);
+});
+
+bayeux.on('disconnect', function(clientId) {
+  console.log('[ DISCONNECT] ' + clientId);
+});
+
+console.log('Listening on ' + port + (secure? ' (https)' : ''));
